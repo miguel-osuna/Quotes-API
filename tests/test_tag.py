319fed2b7a6ad251e8d53f6cf6d518e217960678
Miguel Osuna <osuna.angulo.miguel@gmail.com>
2020-12-21 15:44:43 -0800

    Add test for tag resource

--- tests/test_tag.py
+++ tests/test_tag.py
@@ -1,6 +1,12 @@
 """ Test tag. """
+from flask import url_for
+
+from quotes_api.common import HttpStatus
 
 
 def test_get_all_tags(client, user_headers):
-    pass
+    tags_url = url_for("api.tags")
+    res = client.get(tags_url, headers=user_headers)
+
+    assert res.status_code == HttpStatus.ok_200.value
 
